122a736485e2e0a5d8168c2a18944a5a1e67943b
Joseph-PC <Joseph-PC@DESKTOP-8AP4S74>
2025-05-05 17:37:54 -0500

    Creacion general de entidades en el Shared

---
+++ ModHub.Shared/Entities/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModHub.Shared.Entities
+{
+    internal class Category
+    {
+
+        public int Id { get; set; }
+
+
+        [Display(Name = "Nombre Categoria")] //Pone una Descripcion del campo
+        [Required] //Hace que sea obligatorio
+        [MaxLength(55, ErrorMessage = "Nombre Demasiado largo")]//Caracteres maximos y mensaje de error
+        public string FullName { get; set; }
+
+
+
+
+    }
+}
