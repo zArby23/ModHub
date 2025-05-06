fe92cda19435fb54e493e82ed5395d1735e9ebc4
Joseph-PC <Joseph-PC@DESKTOP-8AP4S74>
2025-05-06 17:09:11 -0500

    Creacion de algunas entidades mas y creacion del dataContext

---
+++ ModHub.Shared/Entities/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModHub.Shared.Entities
+{
+    public class State
+    {
+
+        public int Id { get; set; }
+
+
+        [Display(Name = "Nombre del estado")] //Pone una Descripcion del campo
+        [Required] //Hace que sea obligatorio
+        public string StateName { get; set; }
+
+
+    }
+}
