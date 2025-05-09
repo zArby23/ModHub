085cb041ed423a4ca9c4b93abfd4c8c66ba783ec
Juan Montoya (Arby) <lic.jdavid23@gmail.com>
2025-05-09 17:15:06 -0500

    Creacion de entidad Creator y User

--- ModHub.Shared/Entities/Creator.cs
+++ ModHub.Shared/Entities/Creator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,22 @@ namespace ModHub.Shared.Entities
 {
     public class Creator
     {
+        public int Id { get; set; }
 
+        [Display(Name = "Nombre Usuario")] //Pone una Descripcion del campo
+        [Required] //Hace que sea obligatorio
+        [MaxLength(75, ErrorMessage = "Nombre Demasiado largo")]//Caracteres maximos y mensaje de error
+        public string FullName { get; set; }
 
+        [Display(Name = "Email Usuario")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Correo electrónico no valido")]
+        [MaxLength(130, ErrorMessage = "Email Demasiado largo")]
+        public string Email { get; set; }
 
-
-
+        [Display(Name = "Fecha Registro Usuario")] //Pone una Descripcion del campo
+        [Required] //Hace que sea obligatorio
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public string DateRegistration { get; set; }
     }
 }
--- ModHub.Shared/Entities/User.cs
+++ ModHub.Shared/Entities/User.cs
@@ -4,43 +4,41 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ModHub.Shared.Enums;
 
 namespace ModHub.Shared.Entities
 {
     public class User
     {
-        public int Id { get; set; }
+        [Display(Name = "Documento")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Document { get; set; } = null!;
+
+        [Display(Name = "Nombres")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string FirstName { get; set; } = null!;
+
+        [Display(Name = "Apellidos")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string LastName { get; set; } = null!;
+
+        [Display(Name = "Dirección")]
+        [MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Address { get; set; } = null!;
+
+        [Display(Name = "Foto")]
+        public string Photo { get; set; }
+
+        [Display(Name = "Tipo de usuario")]
+        public UserType UserType { get; set; }
 
 
-        [Display(Name = "Nombre Usuario")] //Pone una Descripcion del campo
-        [Required] //Hace que sea obligatorio
-        [MaxLength(75, ErrorMessage = "Nombre Demasiado largo")]//Caracteres maximos y mensaje de error
-        public string FullName { get; set; }
-
-
-        [Display(Name = "Email Usuario")]
-        [Required]
-        [EmailAddress(ErrorMessage = "Correo electrónico no valido")]
-        [MaxLength(130, ErrorMessage = "Email Demasiado largo")]
-        public string Email { get; set; }
-
-        [Display(Name = "Contraseña Usuario")] //Pone una Descripcion del campo
-        [Required] //Hace que sea obligatorio
-        [MaxLength(75, ErrorMessage = "Contraseña Demasiado larga")]
-        public string Password { get; set; }
-
-        [Display(Name = "Fecha Registro Usuario")] //Pone una Descripcion del campo
-        [Required] //Hace que sea obligatorio
-        [MaxLength(75, ErrorMessage = "Contraseña Demasiado larga")]
-        public string DateRegistration { get; set; }
-
-        [Display(Name = "Fecha Registro Usuario")] //Pone una Descripcion del campo
-        [Required] //Hace que sea obligatorio
-        [MaxLength(75, ErrorMessage = "Contraseña Demasiado larga")]
-        public string DateRegistration { get; set; }
-
-
-
+        [Display(Name = "Usuario")]
+        public string FullName => $"{FirstName} {LastName}";
 
     }
 }
