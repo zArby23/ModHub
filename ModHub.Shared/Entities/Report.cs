179d9fd6f9e76e6e34833960c1a30900977ef404
Juan Montoya (Arby) <lic.jdavid23@gmail.com>
2025-05-09 17:24:01 -0500

    Add Report class and update Foro.cs properties
    - Introduced a new `Report` class in `ModHub.Shared.Entities` with properties for `Id`, `Content`, `Creator`, and `CreatorId`.
    - Added validation attributes to the `Content` property for better data integrity.
    - Updated `PublicationDate` in `Foro.cs` with a connection comment.
    - Included necessary using directives in `Report.cs` for improved functionality.
    - Marked the `Creator` property with `JsonIgnore` to prevent serialization.

--- ModHub.Shared/Entities/Report.cs
+++ ModHub.Shared/Entities/Report.cs
@@ -3,30 +3,30 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ModHub.Shared.Entities
 {
-    public class Foro
+    public class Report
     {
-
-
         public int Id { get; set; }
 
 
-        [Display(Name = "Contenido  Comentario")] //Pone una Descripcion del campo
+        [Display(Name = "Contenido  Reporte")] //Pone una Descripcion del campo
         [Required] //Hace que sea obligatorio
         [MaxLength(1200, ErrorMessage = "Tamaño maximo permitido")]//Caracteres maximos y mensaje de error
-        public string content { get; set; }
+        public string Content { get; set; }
 
 
-        [Display(Name = "FechaPublicacion Comentario")]
-        [Required]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
-        public DateTime PublicationDate { get; set; }
 
-        //Conexion
 
+        //conexiones
+        [JsonIgnore]
+
+        public Creator Creator { get; set; }
+
+        public int CreatorId { get; set; }
 
     }
 }
