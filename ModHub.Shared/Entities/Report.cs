37b9604954d74b15f229a60fd56ec547d87f7466
Joseph-PC <Joseph-PC@DESKTOP-8AP4S74>
2025-05-09 17:13:46 -0500

    Creacion entidad report

---
+++ ModHub.Shared/Entities/Report.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ModHub.Shared.Entities
+{
+    public class Report
+    {
+        public int Id { get; set; }
+
+
+        [Display(Name = "Contenido  Reporte")] //Pone una Descripcion del campo
+        [Required] //Hace que sea obligatorio
+        [MaxLength(1200, ErrorMessage = "Tamaño maximo permitido")]//Caracteres maximos y mensaje de error
+        public string Content { get; set; }
+
+
+
+
+        //conexiones
+        [JsonIgnore]
+
+        public Creator Creator { get; set; }
+
+        public int CreatorId { get; set; }
+
+    }
+}
