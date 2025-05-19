1024aded10dc5f38889cc6cdcd1567ec13ab9bdc
SEBASTIAN RAMIREZ MU�OZ <sebastianramirez1112697@correo.itm.edu.co>
2025-05-19 16:11:55 -0500

    Controller for Report entity

---
+++ ModHub.API/Controllers/ReportsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ModHub.API.Data;
+using ModHub.Shared.Entities;
+
+namespace ModHub.API.Controllers
+{
+    [Route("api/report")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly DataContext _Context;
+
+        public ReportController(DataContext context)
+        {
+            _Context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var report = await _Context.Reports.ToListAsync();
+            return Ok(report);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var post = await _Context.Reports.FirstOrDefaultAsync(x => x.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Report([FromBody] Report report)
+        {
+            _Context.Reports.Add(report);
+            await _Context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = report.Id }, report);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Report report)
+        {
+            _Context.Reports.Update(report);
+            await _Context.SaveChangesAsync();
+            return Ok(report);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var FilasAfectadas = await _Context.Reports
+                    .Where(x => x.Id == id)
+                    .ExecuteDeleteAsync();
+            if (FilasAfectadas == 0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
