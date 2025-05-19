de636548ba596c40bed74526d7a19a3d38156733
EMMANUEL <Emmaca1995@gmail.com>
2025-05-19 16:26:28 -0500

    Controlador de la entidad de Categorias

---
+++ ModHub.API/Controllers/CategoriesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ModHub.API.Data;
+using ModHub.Shared.Entities;
+
+namespace ModHub.API.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategorysController : ControllerBase
+    {
+        private readonly DataContext _Context;
+
+        public CategoriesController(DataContext context)
+        {
+            _Context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await _Context.Categories.ToListAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var categories = await _Context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (categories == null)
+            {
+                return NotFound();
+            }
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Category category)
+        {
+            _Context.Categories.Add(category);
+            await _Context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = category.Id }, category);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Category category)
+        {
+            _Context.Categories.Update(category);
+            await _Context.SaveChangesAsync();
+            return Ok(category);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var FilasAfectadas = await _Context.Categories
+                    .Where(x => x.Id == id)
+                    .ExecuteDeleteAsync();
+            if (FilasAfectadas == 0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
