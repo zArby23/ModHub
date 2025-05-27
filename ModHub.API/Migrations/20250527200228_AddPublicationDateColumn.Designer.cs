9bc237c7517455b890b7200891abb7609ad9f163
Juan Montoya (Arby) <lic.jdavid23@gmail.com>
2025-05-27 15:59:32 -0500

    Add PublicationDate and DateRegistration columns
    This commit introduces two new columns: `PublicationDate` to the `Mods` table and `DateRegistration` to the `Creators` table. Both columns are of type `DateTime`, non-nullable, and have a default value of `new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)`. The `DataContextModelSnapshot.cs` file is also updated to reflect these changes in the Entity Framework Core model.

--- ModHub.API/Migrations/20250527200228_AddPublicationDateColumn.Designer.cs
+++ ModHub.API/Migrations/20250527200228_AddPublicationDateColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ModHub.API.Data;
 
@@ -11,9 +12,11 @@ using ModHub.API.Data;
 namespace ModHub.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250527200228_AddPublicationDateColumn")]
+    partial class AddPublicationDateColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -52,9 +55,6 @@ namespace ModHub.API.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<DateTime>("DateRegistration")
-                        .HasColumnType("datetime2");
 
                     b.Property<string>("Email")
                         .IsRequired()
