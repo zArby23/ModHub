f2f7653159706fa8e541fd38a7db659e6eb1057d
SEBASTIAN RAMIREZ MU�OZ <sebastianramirez1112697@correo.itm.edu.co>
2025-05-28 17:45:40 -0500

    Mejoras en autenticación y autorización en Blazor

--- ModHub.WEB/Program.cs
+++ ModHub.WEB/Program.cs
@@ -1,8 +1,10 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ModHub.WEB;
 using ModHub.WEB.Repositories;
+using ModHub.WEB.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,8 +12,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7777") });
 
+
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7201/") });
+
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+
 builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProvider>();
 
 await builder.Build().RunAsync();
