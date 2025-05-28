55123368b232049e1e9039f44a26ebc90243fa72
SEBASTIAN RAMIREZ MU�OZ <sebastianramirez1112697@correo.itm.edu.co>
2025-05-28 17:43:32 -0500

    Añadir AuthenticationProvider para gestión de autenticación

---
+++ ModHub.WEB/Auth/AuthenticatonProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace ModHub.WEB.Auth
+{
+    public class AuthenticationProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(5000);
+            var oapUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Sebastian"),
+                new Claim("LastName", "Ramirez"),
+                new Claim(ClaimTypes.Name, "159sebastian.ramirez@gmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            authenticationType: "test");
+
+            var anonimous = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(oapUser)));
+        }
+    }
+}
